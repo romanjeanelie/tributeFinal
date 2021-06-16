bfe352646c1884eb2335b466576f3a0239bd8e44
roman7501 <64649303+roman7501@users.noreply.github.com>
2021-06-16 17:35:19 +0200

    add scroll help

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import Sketch from "./js/sketch.js";
 import { gsap } from "gsap";
 
 const startBtn = document.getElementById("start");
-const homePage = document.querySelector(".home");
 
 new Sketch({ dom: document.getElementById("container") });
+
+const tlScroll = gsap.timeline({
+  repeat: 2,
+  paused: true,
+});
+tlScroll.to(".scroll__bar", {
+  scaleY: 1,
+});
+
+tlScroll.to(".scroll__bar", {
+  transformOrigin: "bottom",
+  scaleY: 0,
+});
+tlScroll.to(".scroll__bar", {
+  transformOrigin: "top",
+  scaleY: 0.2,
+});
 
 const tl = gsap.timeline();
 startBtn.addEventListener("click", () => {
@@ -14,5 +30,10 @@ startBtn.addEventListener("click", () => {
   tl.to(".home", {
     duration: 2,
     autoAlpha: 0,
+  });
+  tl.to(".home__help", {
+    duration: 1,
+    autoAlpha: 1,
+    onComplete: () => tlScroll.play(),
   });
 });
